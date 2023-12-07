697290514a5ae836888cdfb01490c5b4db25378a
hao <313347408@qq.com>
2023-12-07 11:45:38 +0800

    hw06&lab10

---
+++ hw/hw06/hw06.scm
@@ -0,0 +1,31 @@
+(define (cddr s)
+  (cdr (cdr s)))
+
+(define (cadr s)
+  'YOUR-CODE-HERE
+  (car (cdr s))
+)
+
+(define (caddr s)
+  'YOUR-CODE-HERE
+  (car (cdr (cdr s)))
+)
+
+
+(define (sign num)
+  'YOUR-CODE-HERE
+  (cond ((< num 0) -1)
+        ((= num 0) 0)
+        (else 1))
+)
+
+
+(define (square x) (* x x))
+
+(define (pow x y)
+  'YOUR-CODE-HERE
+  (cond ((= y 1) x)
+        ((even? y) (square (pow x (quotient y 2))))
+        ((odd? y) (* x (pow x (- y 1)))))
+)
+
